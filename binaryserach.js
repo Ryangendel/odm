161091a889d2207f3496a70075ad9ff74d7d23e1
Ryan <ryan.gendel@bluelabellabs.com>
2022-12-07 12:54:36 -0700

    added notes

--- binaryserach.js
+++ binaryserach.js
@@ -2,13 +2,17 @@ const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 function binarySearch(array, element) {
   let start = 0;
-  let end = array.length - 1;
+  let end = array.length - 1; //9
 
   while (start <= end) {
-    let mid = Math.floor((start + end) / 2);
 
+    //9/2=4.5
+    //mid == 7
+    //array[mid]==8
+    let mid = Math.floor((start + end) / 2);
+    
     if (array[mid] === element) {
-      return true;
+      return mid;
     } else if (array[mid] < element) {
       start = mid + 1;
     } else {
@@ -18,4 +22,4 @@ function binarySearch(array, element) {
   return false;
 }
 
-console.log(binarySearch(arr, 7));
+console.log(binarySearch(arr, 8));
